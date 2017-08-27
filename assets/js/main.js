773a713b0910a06db7c43db52ee4ca686797356c
Youness Hakky <youness.hakky@gmail.com>
2017-08-27 13:51:10 +0100

    before pull

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+
+
+// this is a javascript file
+
+
+(function(){
+
+  alert('this is an alert')
+  console.log('hey console');
+
+  console.error('hey im an error')
+
+  var myObject = new Object();
+
+  myObject.prototype.gather = function (){
+    this.assemble(astuce, greatNumber);
+    
+    docTrig.addEventListener('input change' function(){
+      switch(window.location.hash){
+        case '#':
+          window.reload();
+          break;
+        case '#gathering':
+          window.go(window.host + '/gathering');
+          break;
+        default:
+          console.error(errorMessage)
+          window.go(__404__);
+      }
+    });
+
+    return this.cleverkid || this.stupidKid;
+
+  }
+
+})();
